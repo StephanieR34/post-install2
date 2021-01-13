e943ef07f7c9245c7ff2991f814845f2a9a6af80
StephanieR34 <77360915+StephanieR34@users.noreply.github.com>
2021-01-13 10:53:02 +0100

    Add files via upload
    ajout de mon script en python

---
+++ post-install.py
@@ -0,0 +1,19 @@
+import os
+
+os.system("echo 'UBUNTU POST-INSTALL SCRIPT'"
++"echo 'lol'")
+
+#echo "Updating APT..."
+
+#sudo apt-get update 
+
+#echo "Installing base packages"
+
+#sudo apt-get install --yes git git-extras python3-pip 
+# ici on installe git avec les supplementation git-extras et pip 
+#sudo snap install discord 
+#on installe discord 
+#sudo snap install code --classic
+#on instale vsc 
+
+#clear)
